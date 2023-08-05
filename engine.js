529c5b69c668217f1bab54ba1edac3ffa74a87d1
Applot <cws7833@naver.com>
2023-08-05 21:21:05 +0900

    test22222
    ss

--- engine.js
+++ engine.js
@@ -18,7 +18,7 @@ class TextGame {
     //분기 추가
     //branch: Branch
     addBranch(branch) {
-        this._branchManager.addBranch(branch); // 테스트 주석
+        this._branchManager.addBranch(branch); // 테스트 주석222222222
     }
 
     setTextBarElement(chatBox, nameBox) {
